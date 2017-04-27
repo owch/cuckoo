90ccff0044d53f7bcbf755714e0c7c827a6b4693
Philip Duncan <poduncan@uwaterloo.ca>
2017-04-26 23:15:24 -0700

    sort of working

--- core.js
+++ core.js
@@ -6,7 +6,10 @@ var prevDOM = null;
 
 // Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
-  var srcElement = e.srcElement;
+  var x = e.clientX,
+      y = e.clientY;
+
+  var srcElement = elementFromPoint(x, y);
 
   // Lets check if our underlying element is a DIV.
   if (true) {
@@ -25,3 +28,51 @@ document.addEventListener('mousemove', function (e) {
     prevDOM = srcElement;
   }
 }, false);
+
+document.addEventListener('click', logInner);
+document.removeEventListener('click', logInner);
+
+function logInner(e) {
+    var x = e.clientX,
+      y = e.clientY;
+
+  var srcElement = elementFromPoint(x, y);
+
+  console.log(srcElement.innerHTML);
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+var tagBlacklist = ['IMG'];
+
+function elementFromPoint(x, y) {
+  // http://stackoverflow.com/questions/21051084/javascript-know-all-the-elements-under-your-mouse-pointer-multiple-z-axis-laye
+  var /*x = e.clientX,
+        y = e.clientY,*/
+        // stack = [],
+        elementMouseIsOver = document.elementFromPoint(x, y);
+
+    // stack.push(elementMouseIsOver);
+
+    // console.log("tagName: " + elementMouseIsOver.tagName);
+
+    while (tagBlacklist.includes(elementMouseIsOver.tagName)){
+
+        elementMouseIsOver.style.pointerEvents = 'none';
+        elementMouseIsOver = document.elementFromPoint(x, y);
+
+        // stack.push(elementMouseIsOver);
+    }
+
+    /* Now clean it up */
+    // var i  = 0,
+    //     il = stack.length;
+
+    // for (; i < il; i += 1) {
+    //     stack[i].style.pointerEvents = '';
+    // }
+
+    // console.log(stack);
+
+    return elementMouseIsOver;
+}
